f1a44470d245df588944e2280c9335fcd665ec7e
AliArefnia <maarefnia1377@gmail.com>
2025-08-10 12:40:51 +0330

    install react query and create it's provider and use it in the main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { inter } from "@/ui/fonts";
 import "../styles/globals.css";
+import { ReactQueryProvider } from "@/lib/react-query/provider";
 
 export default function RootLayout({
   children,
@@ -8,7 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
