bb9e33c9874b1262edb0d88c49179ae5d309b056
AliArefnia <maarefnia1377@gmail.com>
2025-08-12 01:24:47 +0330

    create rooms page

---
+++ src/app/rooms/[roomId]/page.tsx
@@ -0,0 +1,13 @@
+import RoomLayout from "./RoomLayout";
+import { getRoomData } from "@/lib/supabase/rooms";
+
+export default async function RoomPage({
+  params,
+}: {
+  params: Promise<{ roomId: string }>;
+}) {
+  const { roomId } = await params;
+  const roomData = await getRoomData(roomId);
+
+  return <RoomLayout roomId={roomId} initialRoomData={roomData}></RoomLayout>;
+}
